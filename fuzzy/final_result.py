8a7cf5549ba6a500ab6fceae9cbca9f96195c92e
Ali Moradzade <alimorizz1379@gmail.com>
2022-06-28 17:42:22 +0430

    adding initial files

---
+++ fuzzy/final_result.py
@@ -0,0 +1,9 @@
+class ProvideResult(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ProvideResult, cls).__new__(cls)
+        return cls.instance
+
+    @staticmethod
+    def get_final_result(input_dict: dict) -> str:
+        pass
