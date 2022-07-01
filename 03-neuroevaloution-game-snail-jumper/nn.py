eb96e4a720ea5a51de11dddab0154751ddd3aed9
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-02 02:02:47 +0430

    adding nn.py, for our neural networks

--- 03-neuroevaloution-game-snail-jumper/nn.py
+++ 03-neuroevaloution-game-snail-jumper/nn.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+def activation(x):
+    """
+    The activation function of our neural network, e.g., Sigmoid, ReLU.
+    :param x: Vector of a layer in our network.
+    :return: Vector after applying activation function.
+    """
+    return 1 / (1 + np.exp(-x))
+
+
 class NeuralNetwork:
 
     def __init__(self, layer_sizes):
@@ -10,17 +19,25 @@ class NeuralNetwork:
         :param layer_sizes: A list containing neuron numbers in each layers. For example [3, 10, 2] means that there are
         3 neurons in the input layer, 10 neurons in the hidden layer, and 2 neurons in the output layer.
         """
-        # TODO (Implement FCNNs architecture here)
-        pass
+        input_layer = layer_sizes[0]
+        hidden_layer = layer_sizes[1]
+        output_layer = layer_sizes[2]
 
-    def activation(self, x):
-        """
-        The activation function of our neural network, e.g., Sigmoid, ReLU.
-        :param x: Vector of a layer in our network.
-        :return: Vector after applying activation function.
-        """
-        # TODO (Implement activation function here)
-        pass
+        w1 = np.random.normal(size=(hidden_layer, input_layer))
+        w2 = np.random.normal(size=(output_layer, hidden_layer))
+
+        b1 = np.random.normal(size=(hidden_layer, 1))
+        b2 = np.random.normal(size=(output_layer, 1))
+
+        self.input_layer = input_layer
+        self.hidden_layer = hidden_layer
+        self.output_layer = output_layer
+
+        self.w1 = w1
+        self.w2 = w2
+
+        self.b1 = b1
+        self.b2 = b2
 
     def forward(self, x):
         """
@@ -28,5 +45,9 @@ class NeuralNetwork:
         :param x: Input vector which is a numpy array.
         :return: Output vector
         """
-        # TODO (Implement forward function here)
-        pass
+        x = x.reshape((self.input_layer, 1))
+
+        a1 = activation(self.w1 @ x + self.b1)
+        a2 = activation(self.w2 @ a1 + self.b2)
+
+        return a2
