44e8e14576a28000ad854adaddc2e9b493e727eb
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-05 18:29:23 +0430

    doing some refactoring

--- 02-fuzzy-diagnosis-of-heart-disease/inference.py
+++ 02-fuzzy-diagnosis-of-heart-disease/inference.py
@@ -7,12 +7,12 @@ def inference(fuzzy_values_dict):
     for rule in rules.rules_list:
         item1 = fuzzy_values_dict[rule[0]]
 
-        if rule[1] == 'nothing':
+        if len(rule) == 2:
             item2 = 1
-        else:
+            output = rule[1]
+        elif len(rule) == 3:
             item2 = fuzzy_values_dict[rule[1]]
-
-        output = rule[2]
+            output = rule[2]
 
         if fuzzy_output_values.get(output) is None:
             fuzzy_output_values[output] = min(item1, item2)
--- 02-fuzzy-diagnosis-of-heart-disease/rules.py
+++ 02-fuzzy-diagnosis-of-heart-disease/rules.py
@@ -25,101 +25,101 @@ rules_list = [
     # Rule 10
     ('bloodPressure_high', 'heartRate_low', 'output_sick1'),
     # Rule 11
-    ('typical_angina', 'nothing', 'output_healthy'),
+    ('typical_angina', 'output_healthy'),
     # Rule 12
-    ('atypical_angina', 'nothing', 'output_sick1'),
+    ('atypical_angina', 'output_sick1'),
     # Rule 13
-    ('non_anginal_pain', 'nothing', 'output_sick2'),
+    ('non_anginal_pain', 'output_sick2'),
     # Rule 14
-    ('asymptomatic', 'nothing', 'output_sick3'),
+    ('asymptomatic', 'output_sick3'),
     # Rule 15
-    ('asymptomatic', 'nothing', 'output_sick4'),
+    ('asymptomatic', 'output_sick4'),
 
     # Rule 16
-    ('female', 'nothing', 'output_sick1'),
+    ('female', 'output_sick1'),
     # Rule 17
-    ('male', 'nothing', 'output_sick2'),
+    ('male', 'output_sick2'),
 
     # Rule 18
-    ('bloodPressure_low', 'nothing', 'output_healthy'),
+    ('bloodPressure_low', 'output_healthy'),
     # Rule 19
-    ('bloodPressure_medium', 'nothing', 'output_sick1'),
+    ('bloodPressure_medium', 'output_sick1'),
     # Rule 20
-    ('bloodPressure_high', 'nothing', 'output_sick2'),
+    ('bloodPressure_high', 'output_sick2'),
     # Rule 21
-    ('bloodPressure_veryhigh', 'nothing', 'output_sick3'),
+    ('bloodPressure_veryhigh', 'output_sick3'),
     # Rule 22
-    ('bloodPressure_veryhigh', 'nothing', 'output_sick4'),
+    ('bloodPressure_veryhigh', 'output_sick4'),
 
     # Rule 23
-    ('cholesterol_low', 'nothing', 'output_healthy'),
+    ('cholesterol_low', 'output_healthy'),
     # Rule 24
-    ('cholesterol_medium', 'nothing', 'output_sick1'),
+    ('cholesterol_medium', 'output_sick1'),
     # Rule 25
-    ('cholesterol_high', 'nothing', 'output_sick2'),
+    ('cholesterol_high', 'output_sick2'),
     # Rule 26
-    ('cholesterol_high', 'nothing', 'output_sick3'),
+    ('cholesterol_high', 'output_sick3'),
     # Rule 27
-    ('cholesterol_veryhigh', 'nothing', 'output_sick4'),
+    ('cholesterol_veryhigh', 'output_sick4'),
 
     # Rule 28
-    ('bloodSugar_veryhigh', 'nothing', 'output_sick2'),
+    ('bloodSugar_veryhigh', 'output_sick2'),
 
     # Rule 29
-    ('ecg_normal', 'nothing', 'output_healthy'),
+    ('ecg_normal', 'output_healthy'),
     # Rule 30
-    ('ecg_normal', 'nothing', 'output_sick1'),
+    ('ecg_normal', 'output_sick1'),
     # Rule 31
-    ('ecg_abnormal', 'nothing', 'output_sick2'),
+    ('ecg_abnormal', 'output_sick2'),
     # Rule 32
-    ('ecg_hypertrophy', 'nothing', 'output_sick3'),
+    ('ecg_hypertrophy', 'output_sick3'),
     # Rule 33
-    ('ecg_hypertrophy', 'nothing', 'output_sick4'),
+    ('ecg_hypertrophy', 'output_sick4'),
 
     # Rule 34
-    ('heartRate_low', 'nothing', 'output_healthy'),
+    ('heartRate_low', 'output_healthy'),
     # Rule 35
-    ('heartRate_medium', 'nothing', 'output_sick1'),
+    ('heartRate_medium', 'output_sick1'),
     # Rule 36
-    ('heartRate_medium', 'nothing', 'output_sick2'),
+    ('heartRate_medium', 'output_sick2'),
     # Rule 37
-    ('heartRate_high', 'nothing', 'output_sick3'),
+    ('heartRate_high', 'output_sick3'),
     # Rule 38
-    ('heartRate_high', 'nothing', 'output_sick4'),
+    ('heartRate_high', 'output_sick4'),
 
     # Rule 39
-    ('exercise_suitable', 'nothing', 'output_sick2'),
+    ('exercise_suitable', 'output_sick2'),
 
     # Rule 40
-    ('oldPeak_low', 'nothing', 'output_healthy'),
+    ('oldPeak_low', 'output_healthy'),
     # Rule 41
-    ('oldPeak_low', 'nothing', 'output_sick1'),
+    ('oldPeak_low', 'output_sick1'),
     # Rule 42
-    ('oldPeak_terrible', 'nothing', 'output_sick2'),
+    ('oldPeak_terrible', 'output_sick2'),
     # Rule 43
-    ('oldPeak_terrible', 'nothing', 'output_sick3'),
+    ('oldPeak_terrible', 'output_sick3'),
     # Rule 44
-    ('oldPeak_risk', 'nothing', 'output_sick4'),
+    ('oldPeak_risk', 'output_sick4'),
 
     # Rule 45
-    ('thallium_normal', 'nothing', 'output_healthy'),
+    ('thallium_normal', 'output_healthy'),
     # Rule 46
-    ('thallium_normal', 'nothing', 'output_sick1'),
+    ('thallium_normal', 'output_sick1'),
     # Rule 47
-    ('thallium_medium', 'nothing', 'output_sick2'),
+    ('thallium_medium', 'output_sick2'),
     # Rule 48
-    ('thallium_high', 'nothing', 'output_sick3'),
+    ('thallium_high', 'output_sick3'),
     # Rule 49
-    ('thallium_high', 'nothing', 'output_sick4'),
+    ('thallium_high', 'output_sick4'),
 
     # Rule 50
-    ('age_young', 'nothing', 'output_healthy'),
+    ('age_young', 'output_healthy'),
     # Rule 51
-    ('age_mild', 'nothing', 'output_sick1'),
+    ('age_mild', 'output_sick1'),
     # Rule 52
-    ('age_old', 'nothing', 'output_2'),
+    ('age_old', 'output_2'),
     # Rule 53
-    ('age_old', 'nothing', 'output_3'),
+    ('age_old', 'output_3'),
     # Rule 54
-    ('age_veryold', 'nothing', 'output_sick4'),
+    ('age_veryold', 'output_sick4'),
 ]
